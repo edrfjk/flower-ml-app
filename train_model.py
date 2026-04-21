e06d7b615cd0ea0846ce0351a039f419bda5484e
Eidref Jake <edrfjk@gmail.com>
2026-04-21 14:21:21 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import joblib
 
 # Load dataset
@@ -7,9 +9,20 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
+# SPLIT DATA (80-20)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
 # Train model
 model = DecisionTreeClassifier()
-model.fit(X, y)
+model.fit(X_train, y_train)
+
+# Evaluate model
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", accuracy)
 
 # Save model
 joblib.dump(model, 'flower_model.joblib')
